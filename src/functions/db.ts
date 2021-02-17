a9e6f654c1ae288dafc1c0480a35262001b52a32
RazerMoon <33849459+RazerMoon@users.noreply.github.com>
2021-02-17 16:45:39 +0000

    logs folder

--- src/functions/db.ts
+++ src/functions/db.ts
@@ -2,8 +2,15 @@ import low from "lowdb"
 import FileSync from "lowdb/adapters/FileSync"
 import { RecordContainer } from "../interfaces/response"
 
+import { existsSync, mkdirSync } from "fs"
+
 export default function initDB(): low.LowdbSync<RecordContainer> {
-  const db = low(new FileSync<RecordContainer>("db.json"))
+  const dir = "./logs"
+  if (!existsSync(dir)) {
+    mkdirSync(dir)
+  }
+
+  const db = low(new FileSync<RecordContainer>("./logs/db.json"))
   db.defaults(<RecordContainer>{ responses: [] }).write()
 
   return db
